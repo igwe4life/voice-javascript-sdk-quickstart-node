9b65284e62cd90c3ac8ec46ff37c93c78f5c3a0e
igwe4life <kingsleyduru@gmail.com>
2022-11-09 12:00:12 +0100

    Update handler.js
    Changed the recording code

--- src/handler.js
+++ src/handler.js
@@ -48,8 +48,11 @@ exports.voiceResponse = function voiceResponse(requestBody) {
     // set the callerId
     //let dial = twiml.dial({ callerId });
     let dial = twiml.dial({
-    action: 'https://call-center.christembassy-ism.com/test/callcenta/callrecprocess.php',
-    method: 'POST' }, callerId);
+       callerId: callerId,
+       record: 'record-from-ringing-dual',
+       method: 'POST',
+       recordingStatusCallback: 'https://call-center.christembassy-ism.com/test/callcenta/callrecprocess.php'
+     });
 
     // Check if the 'To' parameter is a Phone Number or Client Name
     // in order to use the appropriate TwiML noun 
